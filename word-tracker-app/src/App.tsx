b41a745168ea6b3c4de373e5daf5f6a099492c8f
unknown <rajsriselvan.ca@gmail.com>
2024-12-29 00:34:18 +0000

    Tailwind css setup

--- word-tracker-app/src/App.tsx
+++ word-tracker-app/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p>Clean up..</p>
+      <h1 className="text-2xl m-4">Tailwind setup</h1>
     </div>
   );
 }
