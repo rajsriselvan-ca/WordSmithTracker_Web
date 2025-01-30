ec795a5e5a8fbd917b0b45ecfbb060d2cd00b521
unknown <rajsriselvan.ca@gmail.com>
2025-01-30 08:50:48 +0000

    responsive loader

--- word-tracker-app/src/UIComponents/Loader.tsx
+++ word-tracker-app/src/UIComponents/Loader.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-4">
-      <div className="flex items-center">
-        <p className="mr-3">Loading, please wait...</p>
+    <div className="flex flex-col items-center justify-center p-4">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-2">
+        <p className="text-center sm:text-left">Loading, please wait...</p>
         <div className="loader w-6 h-6 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
       </div>
-      <p className="mt-2 text-sm text-gray-600 text-center">
+      <p className="mt-2 text-sm text-gray-600 text-center max-w-[300px] sm:max-w-none">
         Since this app is hosted on a free platform, the server may take a few moments to wake up from inactivity.
       </p>
     </div>
